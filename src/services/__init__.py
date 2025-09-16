7589bb54d798e3d61107f119ed8fdfadbecda1e7
Mothetho <mobiusndou@gmail.com>
2025-09-16 20:29:52 +0200

    solved task has no id error

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,12 +1,12 @@
-from src.services.file_management_service import FileManagementService
-from src.services.ai_service import AIService
-from src.services.compression_service import CompressionService
-from src.services.conversion_service import ConversionService
-from src.services.ocr_service import OCRService
-from src.services.invoice_extraction_service import InvoiceExtractionService
-from src.services.bank_statement_extraction_service import BankStatementExtractionService
-from src.services.export_service import ExportService
+# from src.services.file_management_service import FileManagementService
+# from src.services.ai_service import AIService
+# from src.services.compression_service import CompressionService
+# from src.services.conversion_service import ConversionService
+# from src.services.ocr_service import OCRService
+# from src.services.invoice_extraction_service import InvoiceExtractionService
+# from src.services.bank_statement_extraction_service import BankStatementExtractionService
+# from src.services.export_service import ExportService
 from src.services.service_registry import ServiceRegistry
 
 
-__all__ = ['CompressionService', 'AIService', 'ConversionService', 'FileManagementService', 'OCRService', 'InvoiceExtractionService', 'BankStatementExtractionService', 'ExportService', ServiceRegistry]
+__all__ = ['ServiceRegistry']
